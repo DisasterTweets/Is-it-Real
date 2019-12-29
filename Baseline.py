54140db2dce0e417f840fa58fe5a01ab8dfa18bf
Eddie <Mbfdcg@live.com>
2019-12-29 14:57:31 -0800

    Update Baseline.py

--- Baseline.py
+++ Baseline.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
 import lightgbm as lgb
 import nltk
 import html
@@ -15,15 +14,17 @@ from nltk.tokenize import TweetTokenizer
 from gensim.test.utils import common_texts, get_tmpfile
 from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import f1_score
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
 from collections import Counter
 
 #%% 
+# load stop words from nltk
 stop_words = stopwords.words('english')
 
 
 #%%
+# read training and test data
 train = pd.read_csv('nlp-getting-started/train.csv')
 test = pd.read_csv('nlp-getting-started/test.csv')
 target = train["target"]
@@ -31,6 +32,7 @@ train.drop(['id','keyword','location','target'], axis=1, inplace=True)
 test.drop(['id','keyword','location'], axis=1, inplace=True)
 
 #%% 
+# extract 7 handcraft features before data cleaning procedure
 def handcraftFeat(df):
     basicFeat = pd.DataFrame()
     basicFeat['num_char'] = df['text'].apply(len)
@@ -49,7 +51,7 @@ train_handcraft = handcraftFeat(train)
 test_handcraft = handcraftFeat(test)
 
 #%% 
-
+# data cleaning
 def remove_URL(text):
     url = re.compile(r'https?://\S+|www\.\S+')
     return url.sub(r'',text)
@@ -75,14 +77,15 @@ def textClean(df):
     df['text'] = df['text'].str.lower()
     df['text'] = df['text'].apply(lambda text: remove_URL(text))
     df['text'] = df['text'].apply(lambda text: remove_html(text))
-    df['text'] = df['text'].apply(lambda text: remove_punc(text))
     df['text'] = df['text'].apply(lambda text: token(text))
+    df['text'] = df['text'].apply(lambda text: remove_punc(text))
     return df
 
 train = textClean(train)
 test = textClean(test)
 
 #%%
+# build corpus (list of words of each tweet) from cleaned data 
 def buildCorpus(df):
     corpus = []
     for tweet in df['text']:
@@ -95,6 +98,7 @@ test_corpus = buildCorpus(test)
 
 
 #%% 
+# embedding words using pretrained 100d-GloVec
 embedding_dict = {}
 with open('glove.twitter.27B/glove.twitter.27B.100d.txt', encoding='utf8') as f:
     for line in f:
@@ -135,24 +139,55 @@ def buildDataset(corpus, dic=embedding_dict, vec_length=100, padding_length=25):
 trainValWordFeat = buildDataset(train_corpus, embedding_dict)
 testWordFeat = buildDataset(test_corpus, embedding_dict)
             
-#%% 
-trainValY = np.array(pd.read_csv('nlp-getting-started/train.csv')['target'])
-
 #%%
-# Try flat feature
-XtrainVal = 
-
-#%%
-text1 = '@remy: This is waaaaayyyy too much for you!!!!!!'
-text2 = '@Jason god, good, gooooooooood, goooooddd'
-tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
-print(tknzr.tokenize(text1))
-print(tknzr.tokenize(text2))
-goodVec = embedding_dict['good']
-gooodVec = embedding_dict['goood']
-
-np.sum(np.multiply(goodVec, gooodVec))/(np.linalg.norm(goodVec)*np.linalg.norm(gooodVec))
+# Try flat feature/ feature prepare/ train validation split
+YTrainVal = np.array(pd.read_csv('nlp-getting-started/train.csv')['target'])
+XTrainVal = np.hstack((trainValWordFeat.reshape(train_handcraft.shape[0],-1), np.array(train_handcraft)))
+Xtest = np.hstack((testWordFeat.reshape(test_handcraft.shape[0],-1), np.array(test_handcraft)))
+Xtrain, Xval, ytrain, yval = train_test_split(XTrainVal, YTrainVal, test_size=0.15)
 
 
 #%%
-train_corpus[515]
+# build gradientBoosting model
+params = {"learning_rate": 0.05,
+          "max_depth": 20,
+         "boosting": "gbdt",
+         "bagging_freq": 1,
+         "bagging_fraction": 0.95,
+          "colsample_bytree": 0.5,
+         "min_data_in_leaf": 50,
+         "bagging_seed": 42,
+          "lambda_l2": 0.0001,
+         "metric": "binary_logloss",
+         "random_state": 42}
+
+def evalAcc(pred, dataset):
+    gt = dataset.label
+    pred = (pred > 0.5).astype(int)
+    acc = np.sum(gt == pred)/len(pred)
+    return "Accuracy",acc,True
+
+train_data = lgb.Dataset(Xtrain, label=ytrain)
+val_data = lgb.Dataset(Xval, label=yval)
+clf = lgb.train(params, train_data, num_boost_round=10000,\
+     valid_sets = [train_data, val_data], verbose_eval=100, early_stopping_rounds=100, feval=evalAcc)
+
+
+#%%
+# predict on test set and write predictions into file
+
+def writePredictions(y, filename="submission.csv"):
+    outpred = pd.read_csv("nlp-getting-started/sample_submission.csv")
+    outpred["target"] = y
+    outpred.to_csv(filename,index=False)
+    return 
+
+ypred = (clf.predict(Xtest) > 0.5).astype(int)
+writePredictions(ypred)
+
+
+#%%
+ytest = np.array(pd.read_csv("nlp-getting-started/gt_test.csv")['target'])
+
+#%%
+np.sum(ypred == ytest)/len(ytest)
